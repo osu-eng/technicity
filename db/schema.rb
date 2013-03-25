2ba487958e13d50eed530d9a8e7f27b65e44dbcc
little.129@osu.edu <little.129@osu.edu>
2013-03-25 14:48:41 -0400

    Studies now have a name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325184418) do
+ActiveRecord::Schema.define(:version => 20130325184753) do
 
   create_table "comparisons", :force => true do |t|
     t.integer  "chosen_location_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20130325184418) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.integer  "region_set_id"
+    t.string   "name"
   end
 
   create_table "users", :force => true do |t|
