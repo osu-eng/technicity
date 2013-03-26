827c677780691a92177b600a94572ad6b6fa8101
little.129@osu.edu <little.129@osu.edu>
2013-03-26 16:08:44 -0400

    created study list partial, my stuff controller

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,5 +1,15 @@
 class MyController < ActionController::Base
 
+  def index
+    @studies = Study.all
+    @regions = Region.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @studies }
+    end
+  end
+
   def studies
     @studies = Study.all
 
