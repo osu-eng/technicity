3cb8e12b0481b19b2b3c03ab948de826027d0929
little.129@osu.edu <little.129@osu.edu>
2013-04-01 17:07:45 -0400

    now it works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401165441) do
+ActiveRecord::Schema.define(:version => 20130401183139) do
 
   create_table "comparisons", :force => true do |t|
     t.integer  "chosen_location_id"
