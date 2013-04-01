7e19466bd6f8eb88069338bec14c1585ffe5f3e0
little.129@osu.edu <little.129@osu.edu>
2013-04-01 12:01:41 -0400

    eliminated silly "my" model

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,9 +1,9 @@
 class MyController < ApplicationController
 
   def dashboard
-    @studies = My::studies
-    @region_sets = My::region_sets
-    @regions = My::regions
+    @studies = Study.includes(:comparisons, :region_set => [:regions]).where(:user_id => User::current_id)
+    @region_sets = RegionSet.includes(:studies, :regions ).where(:user_id => User::current_id)
+    @regions = Region.includes(:locations, :region_sets) #.where(:user_id => User::current_id)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -12,7 +12,7 @@ class MyController < ApplicationController
   end
 
   def studies
-    @studies = My::studies
+    @studies = Study.includes(:comparisons, :region_set => [:regions]).where(:user_id => User::current_id)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -21,7 +21,7 @@ class MyController < ApplicationController
   end
 
   def regions
-    @regions = My::regions
+    @regions = Region.includes(:locations, :region_sets) #.where(:user_id => User::current_id)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -30,7 +30,7 @@ class MyController < ApplicationController
   end
 
   def collections
-    @region_sets = My::region_sets
+    @region_sets = RegionSet.includes(:studies, :regions ).where(:user_id => User::current_id)
 
     respond_to do |format|
       format.html # index.html.erb
