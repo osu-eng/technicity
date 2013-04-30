f053fe8a02ae632c7356bf5f6b244cbbd0506069
Corey Hinshaw <hinshaw.25@osu.edu>
2013-04-30 16:34:24 -0400

    Moved location summary to a raw SQL query. Probably not the rails way.

--- app/models/study.rb
+++ app/models/study.rb
@@ -57,4 +57,36 @@ class Study < ActiveRecord::Base
   def max_intensity
     self.region_set.regions.map{|region| region.max_intensity(self.id)}.max
   end
+
+  def results
+    result_set = ActiveRecord::Base.connection.execute("
+    SELECT l.id, l.latitude, l.longitude, l.heading, l.pitch, cl.chosen, rl.rejected
+    FROM locations l
+    LEFT JOIN (
+      SELECT  
+        chosen_location_id as id, 
+        count(*) as chosen
+      FROM comparisons
+      WHERE study_id = #{self.id}
+      GROUP BY chosen_location_id
+    ) cl on cl.id = l.id
+    LEFT JOIN (
+      SELECT  
+        rejected_location_id as id, 
+        count(*) as rejected
+      FROM comparisons
+      WHERE study_id = #{self.id}
+      GROUP BY rejected_location_id
+    ) rl on rl.id = l.id
+    WHERE 
+      l.region_id IN (
+        SELECT r.id
+        FROM regions r
+        JOIN region_set_memberships rsm ON rsm.region_id = r.id
+        JOIN region_sets rs on rs.id = rsm.region_set_id
+        JOIN studies s on s.region_set_id = rs.id
+        WHERE s.id = #{self.id})
+    ")
+    result_set
+  end
 end
