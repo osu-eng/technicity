39d14f1f2f49f95623ed0610a8cad6a6f49ac16b
little.129@osu.edu <little.129@osu.edu>
2013-05-07 16:51:38 -0400

    added study id validation to comparison

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -3,7 +3,7 @@ class Comparison < ActiveRecord::Base
   attr_accessible :chosen_location_id, :rejected_location_id, :remote_ip, :study_id
 
   include ActiveModel::Validations
-  validates_presence_of :chosen_location_id, :rejected_location_id, :voter_remote_ip, :voter_session_id
+  validates_presence_of :chosen_location_id, :rejected_location_id, :voter_remote_ip, :voter_session_id, :study_id
 
   belongs_to :study
   belongs_to :chosen_location, :class_name => 'Location', :foreign_key => 'chosen_location_id'
