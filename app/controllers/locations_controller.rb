e026a6ec73c2117e7ed3966fd1643bfff9a25af6
little.129@osu.edu <little.129@osu.edu>
2013-05-08 12:22:37 -0400

    added ownership check back to create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -44,6 +44,9 @@ class LocationsController < ApplicationController
   # POST /locations.json
   def create
     @location = Location.new(params[:location])
+    if current_user.admin || (@location.region.user != current_user)
+      trigger_403('You can not add a location to this region')
+    end
 
     respond_to do |format|
       if @location.save
