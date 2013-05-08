f9c87ec34fead00c848be6608b576dc3504d8239
little.129@osu.edu <little.129@osu.edu>
2013-05-08 10:50:26 -0400

    removed study.show

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -41,14 +41,14 @@ class StudiesController < ApplicationController
 
   # GET /studies/1
   # GET /studies/1.json
-  def show
-    @study = Study.find(params[:id])
+  # def show
+  #  @study = Study.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @study }
-    end
-  end
+  #  respond_to do |format|
+  #    format.html # show.html.erb
+  #    format.json { render json: @study }
+  #  end
+  # end
 
   # GET /studies/1/results
   # GET /studies/1/results.json
