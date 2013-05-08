6c117736f3d532d89129a6bd74bd2806a5f0e951
Corey Hinshaw <hinshaw.25@osu.edu>
2013-05-08 10:57:24 -0400

    The home page will now show random studies when nothing has been promoted

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     @study = Study.randomPromotedActive
+    if @study.blank?
+      @study = Study.randomActive
+    end
   end
 
   def help
