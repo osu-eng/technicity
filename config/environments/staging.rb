87874c4c476345e2f2239e92ce304a331bbc691e
Corey Hinshaw <hinshaw.25@osu.edu>
2013-05-10 14:31:17 -0400

    Added action mailer configuration for staging and production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,7 +64,7 @@ Technicity::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+
   # Needed for devise
-  config.action_mailer.default_url_options = { :host => 'streetseen.osu.edu' }
+  config.action_mailer.default_url_options = { :host => 'streetseen.qa.engineering.osu.edu' }
 end
