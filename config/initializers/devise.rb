ef0e2eee660a021713b48d0dec4369d13f609e51
little.129@osu.edu <little.129@osu.edu>
2013-12-29 20:23:03 -0500

    corrected devise secret token (hopefully)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.secret_key = SECRET_TOKEN
+  config.secret_key = ENV["SECRET_TOKEN"]
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
